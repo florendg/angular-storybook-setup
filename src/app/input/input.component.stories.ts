571832f0edabcba1b0a6e9a19bc6662f06ef8177
floren <florendg@gmail.com>
2025-01-03 08:16:04 +0100

    try to get storybook up and running

--- src/app/input/input.component.stories.ts
+++ src/app/input/input.component.stories.ts
@@ -1,16 +1,9 @@
-import { Meta, Story } from "@storybook/angular";
+import {Meta, StoryObj} from "@storybook/angular";
 import { InputComponent } from "./input.component";
-import { Title,Subtitle,Description,Primary,ArgsTable,PRIMARY_STORY,Stories } from "@storybook/addon-docs";
 
 export default {
   title: 'General/Input',
   component: InputComponent,
 } as Meta;
-
-const Template: Story = (args) => ({
-  props: args
-});
-
-export const Default: Story = Template.bind({});
 
 
